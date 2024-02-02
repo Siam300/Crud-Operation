71435b102cbedf05c6f5b29da13fed4b642636d4
Siam300 <89306483+Siam300@users.noreply.github.com>
2024-02-03 04:12:12 +0600

    Get and Post method implemented

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    
+    path('products', views.product_list, name = 'products')
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from . models import Product
+from . serializers import ProductSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+
+        #Get all of the produts or list of all products
+        products = Product.objects.all()
+
+        serializer = ProductSerializer(products, many = True)
+        
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
+        
